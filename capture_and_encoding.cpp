764899f235fdf8c931a9217ffd15b5816c0748ae
Amin Husni <aminhusni@gmail.com>
2019-10-15 01:31:19 +0800

    Added OSD support and removed IR/Night control

--- capture_and_encoding.cpp
+++ capture_and_encoding.cpp
@@ -1,14 +1,30 @@
 #include <string.h>    
 #include <errno.h>      
 
+#include <inttypes.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include <imp/imp_isp.h>
 #include <imp/imp_system.h>
 #include <imp/imp_log.h>
 #include <imp/imp_framesource.h>
 #include <imp/imp_encoder.h>
+#include <imp/imp_osd.h>
 
 #include "imp-common.h" 
 #include "capture_and_encoding.h"
+
+#ifdef SUPPORT_RGB555LE
+#include "bgramapinfo_rgb555le.h"
+#else
+#include "bgramapinfo.h"
+#endif
 
 #define TAG "capture_and_encoding"
 
@@ -18,6 +34,9 @@ extern "C" {
 extern int IMP_OSD_SetPoolSize(int newPoolSize);
 extern int IMP_Encoder_SetPoolSize(int newPoolSize0);
 }
+
+int grpNum = 0;
+IMPRgnHandle *prHander;
 
 int destory()
 {
@@ -142,6 +161,96 @@ void *get_stream(int fd, int chn)
 	return 0;
 }
 
+
+// THE OSD PARTS STARTS HERE
+
+static int osd_show(void)
+{
+        int ret;
+
+        ret = IMP_OSD_ShowRgn(prHander[0], grpNum, 1);
+        if (ret != 0) {
+                IMP_LOG_ERR(TAG, "IMP_OSD_ShowRgn() timeStamp error\n");
+                return -1;
+        }
+
+        return 0;
+}
+
+static void *update_thread(void *p)
+{
+        int ret;
+
+        /*generate time*/
+        char DateStr[40];
+        time_t currTime;
+        struct tm *currDate;
+        unsigned i = 0, j = 0;
+        void *dateData = NULL;
+        uint32_t *data = (uint32_t *)p;
+        IMPOSDRgnAttrData rAttrData;
+
+        ret = osd_show();
+        if (ret < 0) {
+                IMP_LOG_ERR(TAG, "OSD show error\n");
+                return NULL;
+        }
+
+        while(1) {
+                        int penpos_t = 0;
+                        int fontadv = 0;
+
+                        time(&currTime);
+                        currDate = localtime(&currTime);
+                        memset(DateStr, 0, 40);
+                        strftime(DateStr, 40, "%Y-%m-%d %H:%M:%S", currDate);
+                        for (i = 0; i < 20; i++) {
+                                switch(DateStr[i]) {
+                                        case '0' ... '9':
+                                                dateData = (void *)gBgramap[DateStr[i] - '0'].pdata;
+                                                fontadv = gBgramap[DateStr[i] - '0'].width;
+                                                penpos_t += gBgramap[DateStr[i] - '0'].width;
+                                                break;
+                                        case '-':
+                                                dateData = (void *)gBgramap[10].pdata;
+                                                fontadv = gBgramap[10].width;
+                                                penpos_t += gBgramap[10].width;
+                                                break;
+                                        case ' ':
+                                                dateData = (void *)gBgramap[11].pdata;
+                                                fontadv = gBgramap[11].width;
+                                                penpos_t += gBgramap[11].width;
+                                                break;
+                                        case ':':
+                                                dateData = (void *)gBgramap[12].pdata;
+                                                fontadv = gBgramap[12].width;
+                                                penpos_t += gBgramap[12].width;
+                                                break;
+                                        default:
+                                                break;
+                                }
+#ifdef SUPPORT_RGB555LE
+                                for (j = 0; j < OSD_REGION_HEIGHT; j++) {
+                                        memcpy((void *)((uint16_t *)data + j*20*OSD_REGION_WIDTH + penpos_t),
+                                                        (void *)((uint16_t *)dateData + j*fontadv), fontadv*2);
+                                }
+#else
+                                for (j = 0; j < OSD_REGION_HEIGHT; j++) {
+                                        memcpy((void *)((uint32_t *)data + j*20*OSD_REGION_WIDTH + penpos_t),
+                                                        (void *)((uint32_t *)dateData + j*fontadv), fontadv*4);
+                                }
+
+#endif
+                        }
+                        rAttrData.picData.pData = data;
+                        IMP_OSD_UpdateRgnAttrData(prHander[0], &rAttrData);
+
+                        sleep(1);
+        }
+
+        return NULL;
+}
+
 int capture_and_encoding()
 {
 	int ret = 0;
@@ -184,10 +293,32 @@ int capture_and_encoding()
 		return -1;
 	}
 
+	// Create the group for the OSD
+	if (IMP_OSD_CreateGroup(0) < 0) {
+			IMP_LOG_ERR(TAG, "IMP_OSD_CreateGroup(0) error !\n");
+			return -1;
+	}
+
+	// Initialize the OSD that we want to use here
+	prHander = sample_osd_init(grpNum);
+			if (prHander <= 0) {
+					IMP_LOG_ERR(TAG, "OSD init failed\n");
+					return -1;
+	}
+
+
 	/* Step.4 Bind */
+	// Create a bind chain for the framesource and OSD
+	IMPCell osdcell = {DEV_ID_OSD, 0, 0};
+	ret = IMP_System_Bind(&chn[0].framesource_chn, &osdcell);
+	if (ret < 0) {
+			IMP_LOG_ERR(TAG, "Bind FrameSource channel0 and OSD failed\n");
+			return -1;
+	}
+	// Then bind the previous chain which is OSD with the encoder
 	for (i = 0; i < FS_CHN_NUM; i++) {
 		if (chn[i].enable) {
-			ret = IMP_System_Bind(&chn[i].framesource_chn, &chn[i].imp_encoder);
+			ret = IMP_System_Bind(&osd_cell, &chn[i].imp_encoder);
 			if (ret < 0) {
 				IMP_LOG_ERR(TAG, "Bind FrameSource channel%d and Encoder failed\n",i);
 				return -1;
@@ -195,16 +326,31 @@ int capture_and_encoding()
 		}
 	}
 
-	/* Step.5 Stream On */
+		/* Step.5 Create OSD bgramap update thread */
+	pthread_t tid;
+#ifdef SUPPORT_RGB555LE
+	uint32_t *timeStampData = malloc(20 * OSD_REGION_HEIGHT * OSD_REGION_WIDTH * 2);
+#else
+	uint32_t *timeStampData = (uint32_t *)malloc(20 * OSD_REGION_HEIGHT * OSD_REGION_WIDTH * 4);
+#endif
+	if (timeStampData == NULL) {
+			IMP_LOG_ERR(TAG, "valloc timeStampData error\n");
+			return -1;
+	}
+
+	ret = pthread_create(&tid, NULL, update_thread, timeStampData);
+	if (ret) {
+			IMP_LOG_ERR(TAG, "thread create error\n");
+			return -1;
+	}
+
+	/* Step.6 Stream On */
 	ret = sample_framesource_streamon();
 	if (ret < 0) {
 		IMP_LOG_ERR(TAG, "ImpStreamOn failed\n");
 		return -1;
 	}
 
-	// start thread for activating night mode & IR cut filter
-	pthread_t thread_info;
-	pthread_create(&thread_info, NULL, sample_soft_photosensitive_ctrl, NULL);
 
 	return 0;
 }
